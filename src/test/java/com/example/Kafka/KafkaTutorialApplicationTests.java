b4871e582bc27a8763ebad9bf78fae5c25689819
yasserqamhiah <yaserqamhiah@gmail.com>
2023-06-26 11:48:40 +0300

    Kafka Consumer

---
+++ src/test/java/com/example/Kafka/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
